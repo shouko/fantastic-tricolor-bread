d3a615dd7c29a142adff24f21333ef028c25f265
shouko <imshouko@gmail.com>
2020-06-28 07:27:19 +0900

    Add mutation observer

--- src/content.js
+++ src/content.js
@@ -31,4 +31,14 @@ async function init() {
   if (tracks.length > 0) trackSetting.change(0);
 }
 
+const observer = new MutationObserver((mutations) => {
+  const matches = mutations.some((mut) => {
+    if (mut.target.localName !== 'video') return false;
+    return mut.target.classList.contains('VideoPlayer__Video');
+  });
+  if (matches) init();
+});
+
+observer.observe(document.querySelector('main'), { attributes: true, subtree: true });
+
 init();
