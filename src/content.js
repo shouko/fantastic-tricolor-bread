6bfeeef4a342feaa91a81cc734ff05b542a2ad5e
shouko <imshouko@gmail.com>
2020-06-28 05:09:48 +0900

    Refactor

--- src/content.js
+++ src/content.js
@@ -1,4 +1,118 @@
+/* eslint-disable max-classes-per-file */
 /* eslint-disable no-console */
+
+let trackSelector = null;
+let trackSelectorActivator = null;
+
+class TrackSelector {
+  constructor(tracks) {
+    this.list = document.createElement('ul');
+    tracks.forEach((track) => {
+      this.list.appendChild(TrackSelector.createItem(track));
+    });
+
+    this.el = TrackSelector.createContainer();
+    this.el.append(this.list);
+  }
+
+  static createContainer() {
+    const title = document.createElement('div');
+    title.classList.add('VideoPlayer__QualitySettingTitle');
+    title.innerText = '字幕';
+
+    const e = document.createElement('div');
+    e.classList.add('VideoPlayer__QualitySetting');
+    e.style.marginRight = '50px';
+    e.appendChild(title);
+    return e;
+  }
+
+  static createItem(track) {
+    const baseClassName = 'VideoPlayer__QualitySelector';
+    const activeClassName = `${baseClassName}--active`;
+    const t = document.createElement('li');
+    t.dataset.id = track.id;
+    t.innerText = track.label;
+    t.classList.add(baseClassName);
+    t.addEventListener('click', (e) => {
+      // TODO: Switch subtitle track to current
+      const parent = e.target.parentElement;
+      if (parent) {
+        const siblings = parent.children;
+        for (let i = 0; i < siblings.length; i += 1) {
+          siblings[i].classList.remove(activeClassName);
+        }
+      }
+      e.target.classList.add(activeClassName);
+    });
+    return t;
+  }
+
+  inject() {
+    if (document.contains(this.el)) return;
+    const playerContainer = document.querySelector('[ref=videoPlayerContainer]');
+    const qualitySelector = document.querySelector('[ref=videoPlayerContainer] > .VideoPlayer__QualitySetting');
+    if (!playerContainer || !qualitySelector) return;
+    playerContainer.insertBefore(this.el, qualitySelector);
+  }
+}
+
+class TrackSelectorActivator {
+  constuctor() {
+    const icon = TrackSelectorActivator.createIcon();
+    const iconContainer = TrackSelectorActivator.createIconContainer();
+    iconContainer.appendChild(icon);
+
+    const e = document.createElement('div');
+    e.classList.add('VideoPlayer__GearIcon');
+    e.addEventListener('click', () => {
+      // toggle track selector
+    });
+    e.appendChild(iconContainer);
+    this.el = e;
+  }
+
+  static createIcon() {
+    /*
+    const path = document.createElement('path');
+    // TODO: Add real icon
+    path.setAttribute('d', 'FOOBAR');
+
+    const svg = document.createElement('svg');
+    svg.setAttribute('viewBox', '0 0 20 20');
+    svg.appendChild(path);
+    */
+
+    const el = document.createElement('div');
+    el.innerText = '字';
+    const style = {
+      margin: '0 auto',
+      height: '30px',
+      width: '20px',
+      fontSize: '20px',
+    };
+    Object.entries(style).forEach(([key, val]) => {
+      el.style[key] = val;
+    });
+    return el;
+  }
+
+  static createIconContainer() {
+    const el = document.createElement('div');
+    el.classList.add('SvgIcon');
+    el.classList.add('SvgIcon--default');
+    el.classList.add('SvgIcon--medium');
+    return el;
+  }
+
+  inject() {
+    if (document.contains(this.el)) return;
+    const p = document.querySelector('.VideoPlayer__Option');
+    const next = document.querySelector('.VideoPlayer__Option > .VideoPlayer__GearIcon');
+    if (!p || !next) return;
+    p.insertBefore(this.el, next);
+  }
+}
 
 function createSubtitleTrack(opts) {
   const s = document.createElement('track');
@@ -36,84 +150,6 @@ async function fetchSubtitlesData(eid) {
  */
 }
 
-function createTrackSelectorItem(track) {
-  const t = document.createElement('li');
-  t.dataset.id = track.id;
-  t.innerText = track.label;
-  t.classList.add('VideoPlayer__QualitySelector');
-  return t;
-}
-
-function createTrackSelector(tracks) {
-  const title = document.createElement('div');
-  title.classList.add('VideoPlayer__QualitySettingTitle');
-  title.innerText = '字幕';
-
-  const ul = document.createElement('ul');
-  tracks.forEach((track) => {
-    ul.appendChild(createTrackSelectorItem(track));
-  });
-
-  const e = document.createElement('div');
-  e.classList.add('VideoPlayer__QualitySetting');
-  e.appendChild(title);
-  e.appendChild(ul);
-  return e;
-}
-
-function injectTrackSelector(el) {
-  const p = document.querySelector('[ref=videoPlayerContainer]');
-  const next = document.querySelector('[ref=videoPlayerContainer] > .VideoPlayer__QualitySetting');
-  if (!p || !next) return;
-  p.insertBefore(el, next);
-}
-
-function createTrackSelectorActivator() {
-  /*
-  const path = document.createElement('path');
-  // TODO: Add real icon
-  path.setAttribute('d', 'FOOBAR');
-
-  const svg = document.createElement('svg');
-  svg.setAttribute('viewBox', '0 0 20 20');
-  svg.appendChild(path);
-  */
-
-  const svg = document.createElement('div');
-  svg.innerText = '字';
-  const style = {
-    margin: '0 auto',
-    height: '30px',
-    width: '20px',
-    fontSize: '20px',
-  };
-  Object.entries(style).forEach(([key, val]) => {
-    svg.style[key] = val;
-  });
-
-  const svgContainer = document.createElement('div');
-  svgContainer.classList.add('SvgIcon');
-  svgContainer.classList.add('SvgIcon--default');
-  svgContainer.classList.add('SvgIcon--medium');
-
-  const e = document.createElement('div');
-  e.classList.add('VideoPlayer__GearIcon');
-  e.addEventListener('click', () => {
-    // toggle track selector
-  });
-
-  svgContainer.appendChild(svg);
-  e.appendChild(svgContainer);
-  return e;
-}
-
-function injectTrackSelectorActivator(el) {
-  const p = document.querySelector('.VideoPlayer__Option');
-  const next = document.querySelector('.VideoPlayer__Option > .VideoPlayer__GearIcon');
-  if (!p || !next) return;
-  p.insertBefore(el, next);
-}
-
 async function init() {
   const eid = document.querySelector('[ref=video]').getAttribute('episodeid');
   const tracksMeta = await fetchSubtitlesData(eid).map();
@@ -128,8 +164,10 @@ async function init() {
   tracks.forEach((t) => {
     injectSubtitleTrack(createSubtitleTrack(t));
   });
-  injectTrackSelector(createTrackSelector(tracks));
-  injectTrackSelectorActivator(createTrackSelectorActivator());
+  if (!trackSelector) trackSelector = new TrackSelector();
+  trackSelector.inject();
+  if (!trackSelectorActivator) trackSelectorActivator = new TrackSelectorActivator();
+  trackSelectorActivator.inject();
 }
 
 init();
